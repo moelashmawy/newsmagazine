bc93422221be33fbfe276ff074174d40f822939d
hamohuh <hamodroid@gmail.com>
2020-04-07 21:43:23 +0200

    Updates

---
+++ precache-manifest.934393be30ee58cf479a351e62dec400.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9693932d0d7a4173f577aed286199a",
+    "url": "/newsmagazine/index.html"
+  },
+  {
+    "revision": "8d7dd43ddc5926cfb3f1",
+    "url": "/newsmagazine/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "10de10ee1a841da163ab",
+    "url": "/newsmagazine/static/css/main.7f108343.chunk.css"
+  },
+  {
+    "revision": "8d7dd43ddc5926cfb3f1",
+    "url": "/newsmagazine/static/js/2.73db7b90.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/newsmagazine/static/js/2.73db7b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10de10ee1a841da163ab",
+    "url": "/newsmagazine/static/js/main.3a77b8b9.chunk.js"
+  },
+  {
+    "revision": "ed55a0ef73561e345c81",
+    "url": "/newsmagazine/static/js/runtime-main.461fedc0.js"
+  }
+]);
